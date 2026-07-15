6ff88e937956a27d75c4cc58222f50ae0a8c654e
= <maxigroxx@gmail.com>
2026-07-15 14:18:38 -0300

    agrego grid que se usara a modo de file manager

--- src/sktsender-window.c
+++ src/sktsender-window.c
@@ -24,25 +24,36 @@
 
 struct _SktsenderWindow
 {
-	GtkApplicationWindow  parent_instance;
-
-	/* Template widgets */
-	GtkLabel            *label;
+  GtkApplicationWindow parent_instance;
+  /* Template widgets */
+  GtkGridView *grid;
 };
 
 G_DEFINE_FINAL_TYPE (SktsenderWindow, sktsender_window, GTK_TYPE_APPLICATION_WINDOW)
 
+
 static void
 sktsender_window_class_init (SktsenderWindowClass *klass)
 {
-	GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
-	gtk_widget_class_set_template_from_resource (widget_class, "/com/github/anachuri/sktSender/sktsender-window.ui");
-	gtk_widget_class_bind_template_child (widget_class, SktsenderWindow, label);
+  gtk_widget_class_set_template_from_resource (widget_class, "/com/github/anachuri/sktSender/sktsender-window.ui");
+  // gtk_widget_class_bind_template_child (widget_class, SktsenderWindow, label);
+
 }
 
 static void
 sktsender_window_init (SktsenderWindow *self)
 {
-	gtk_widget_init_template (GTK_WIDGET (self));
+  gtk_widget_init_template (GTK_WIDGET (self));
+
+  // gtk_widget_class_bind_template_child (widget_class, SktsenderWindow, self->grid);
+
+  GFile *file = g_file_new_for_path (".");
+  GtkDirectoryList *dl = gtk_directory_list_new ("standard::name", file);
+  g_object_unref (file);
+  GtkNoSelection *model = gtk_no_selection_new (G_LIST_MODEL (dl));
+  gtk_grid_view_set_model (self->grid, GTK_SELECTION_MODEL (model));
 }
+
+
