c78fa64faea54cc544eabdfd9611a02aa8ab756d
= <maxigroxx@gmail.com>
2026-08-01 10:27:56 -0300

    agrego cambios en la creacion de items en el grid, reemplazola creacion por medio de gtkBuilder por GtkSignalFactory

--- src/sktsender-window.c
+++ src/sktsender-window.c
@@ -44,19 +44,44 @@ get_icon (GtkListItem *item, GFileInfo *info){
   return icon ? g_object_ref (icon) : NULL;
 }
 
-/* static void
-setup_cb (GtkSignalListItemFactory *self, GtkListItem *listitem, gpointer user_data) {
-  GtkWidget *lb = gtk_label_new (NULL);
-  gtk_list_item_set_child (listitem, lb);
-  /* Because gtk_list_item_set_child sunk the floating reference of lb, releasing (unref) isn't necessary for lb.
-} */
+static void
+setup_listitem_cb (GtkListItemFactory *factory,
+                   GtkListItem        *list_item){
+  GtkWidget *box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,10);
+  GtkWidget *image;
+  GtkWidget *label = gtk_label_new(NULL);
+  image = gtk_image_new ();
+  gtk_image_set_icon_size (GTK_IMAGE (image), GTK_ICON_SIZE_LARGE);
+  gtk_box_append (GTK_BOX (box), image);
+  gtk_box_append (GTK_BOX (box), label);
+  gtk_list_item_set_child (list_item, box);
 
-/*static void bind_cb (GtkListItemFactory *self, GtkListItem *listitem, gpointer user_data) {
-  GtkWidget *widget = gtk_list_item_get_child (listitem);
+}
+
+static void pressed_cb (GtkGestureClick *gesture,
+         int              n_press,
+         double           x,
+         double           y,
+         GtkWidget       *area)
+{
+  printf ("hila pololola\n");
+}
+
+static void
+bind_listitem_cb (GtkListItemFactory *factory, GtkListItem *list_item)
+{
+  GtkWidget *box;
+  //GFile *file;
+  box = gtk_list_item_get_child (list_item);
+ // file = gtk_list_item_get_item (list_item);
+ // gtk_image_set_from_gicon (GTK_IMAGE (image),g_app_info_get_icon (app_info));
+
   GtkGesture *gesture = gtk_gesture_click_new ();
-  gtk_widget_add_controller (widget,GTK_EVENT_CONTROLLER(gesture));
-  printf("hi\n");
-}*/
+  gtk_gesture_single_set_button (GTK_GESTURE_SINGLE (gesture), GDK_BUTTON_SECONDARY);
+  gtk_widget_add_controller (box, GTK_EVENT_CONTROLLER (gesture));
+  g_signal_connect (gesture, "pressed", G_CALLBACK (pressed_cb), box);
+}
+
 
 static void
 grid_activate (GtkGridView *grid, int position, gpointer user_data) {
@@ -76,68 +101,40 @@ sktsender_window_class_init (SktsenderWindowClass *klass)
   gtk_widget_class_bind_template_callback (widget_class, get_icon);
 }
 
-void items_changed (
-  GListModel* self,
-  guint position,
-  guint removed,
-  guint added,
-  gpointer user_data){
-
-    printf("hola\n");
-  }
-
-
 static void sktsender_window_init (SktsenderWindow *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
   GFile *file = g_file_new_for_path ("/home/imaxii");
   GtkDirectoryList *dl = gtk_directory_list_new ("standard::name", file);
-  guint n_items = g_list_model_get_n_items (G_LIST_MODEL (dl));
   g_object_unref (file);
-
   GtkSingleSelection *model = gtk_single_selection_new (G_LIST_MODEL (dl));
   const char *ui_string =
-      "<interface>"
-      "  <template class=\"GtkListItem\">"
-      "    <property name=\"child\">"
-      "      <object class=\"GtkBox\">"
-      "        <property name=\"orientation\">GTK_ORIENTATION_VERTICAL</property>"
-      "        <property name=\"spacing\">20</property>"
-      "        <child>"
-      "          <object class=\"GtkImage\">"
-      "            <property name=\"icon-size\">GTK_ICON_SIZE_LARGE</property>"
-      "            <binding name=\"gicon\">"
-      "              <closure type=\"GIcon\" function=\"get_icon\">"
-      "                <lookup name=\"item\">GtkListItem</lookup>"
-      "              </closure>"
-      "            </binding>"
-      "          </object>"
-      "        </child>"
-      "        <child>"
-      "          <object class=\"GtkLabel\">"
-      "            <property name=\"hexpand\">TRUE</property>"
-      "            <property name=\"xalign\">0.5</property>"
-      "            <binding name=\"label\">"
-      "              <closure type=\"gchararray\" function=\"get_file_name\">"
-      "                <lookup name=\"item\">GtkListItem</lookup>"
-      "              </closure>"
-      "            </binding>"
-      "          </object>"
-      "        </child>"
-      "      </object>"
-      "    </property>"
-      "  </template>"
-      "</interface>";
+"<interface>"
+  "<template class=\"GtkListItem\">"
+    "<property name=\"child\">"
+      "<object class=\"GtkLabel\">"
+        "<binding name=\"label\">"
+          "<closure type=\"gchararray\" function=\"get_file_name\">"
+            "<lookup name=\"item\">GtkListItem</lookup>"
+          "</closure>"
+        "</binding>"
+      "</object>"
+    "</property>"
+  "</template>"
+"</interface>"
+;
+  //GBytes *gbytes = g_bytes_new_static (ui_string, strlen (ui_string));
+  //GtkListItemFactory *factory = gtk_builder_list_item_factory_new_from_bytes (NULL, gbytes);
+  GtkListItemFactory *factory = gtk_signal_list_item_factory_new ();
+  g_signal_connect (factory, "setup", G_CALLBACK (setup_listitem_cb), NULL);
+  g_signal_connect (factory, "bind", G_CALLBACK (bind_listitem_cb), NULL);
 
-  GBytes *gbytes = g_bytes_new_static (ui_string, strlen (ui_string));
-  GtkListItemFactory *factory = gtk_builder_list_item_factory_new_from_bytes (NULL, gbytes);
+
   gtk_grid_view_set_factory (self->grid, factory);
   gtk_grid_view_set_model (self->grid, GTK_SELECTION_MODEL (model));
   g_object_ref (self->grid);
-
- // g_signal_connect (factory, "bind", G_CALLBACK (bind_cb), NULL);
-    g_signal_connect (GTK_GRID_VIEW (self->grid), "activate", G_CALLBACK (grid_activate), NULL);
-  g_signal_connect (G_LIST_MODEL(gtk_grid_view_get_model(self->grid)), "items-changed", G_CALLBACK (items_changed), NULL);
-
+  g_signal_connect (GTK_GRID_VIEW (self->grid), "activate", G_CALLBACK (grid_activate), NULL);
 }
+
+
 
